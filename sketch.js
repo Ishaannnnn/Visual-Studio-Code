7d6098a4dea24418dc54d3cb7c3d6a5809aaa05c
Ishaannnnn <110273048+Ishaannnnn@users.noreply.github.com>
2022-07-30 09:37:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("My name is Ishaan");
+console.log("I Study in THE KHAITAN SCHOOL");
+console.log("I am 11 years old");
+console.log("My hobbies are CODING,BADMINTON,FIFA22");
